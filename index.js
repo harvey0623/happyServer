02e93e0e7c751c99b14210e8c1ccfa1df3624f37
harvey <shoot0988@gmail.com>
2020-07-05 18:55:10 +0800

    fix

--- index.js
+++ index.js
@@ -4,8 +4,6 @@ const cookieParser = require('cookie-parser')
 const app = express();
 const cors = require('cors');
 const port = process.env.PORT || 3034;
-const harveynuxt = require('./route/harveynuxt/index.js');
-const cal = require('./route/cal/index.js');
 const happy = require('./route/happy/index.js');
 require('dotenv').config({
    path: process.env.NODE_ENV === 'dev' ? '.env.dev' : '.env'
@@ -17,6 +15,4 @@ app.use(cookieParser());
 app.use(cors());
 app.listen(port);
 
-app.use('/harveynuxt', harveynuxt);
-app.use('/cal', cal);
 app.use('/happy', happy);
