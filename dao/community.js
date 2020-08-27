2197c047269f90a80876440a55e14d5472334ffd
harvey <harvey@henghung.com>
2020-08-27 14:18:02 +0800

    將notify的api移到community

--- dao/community.js
+++ dao/community.js
@@ -1,6 +1,6 @@
 const axios = require('axios');
 module.exports = {
-   async bulletin(payload) {
+   async getBulletin(payload) {
       let result = await axios({
          url: 'https://hj-platform.dev.pin2wall.com/api/app/community/announcement_list',
          method: 'post',
@@ -14,6 +14,23 @@ module.exports = {
             let imgUrl = item.iPosition === 1 ? '1000x150' : '120x120';
             item.vImage = `https://fakeimg.pl/${imgUrl}`;
          })
+         return res.data;
+      }).catch(err => {
+         console.log(err);
+      });
+      return result;
+   },
+   async getNotify(payload) {
+      let result = await axios({
+         url: 'https://hj-platform.dev.pin2wall.com/api/app/community/notify_list',
+         method: 'post',
+         data: {
+            iUserId: payload.iUserId,
+            vToken : payload.vToken,
+            iCommunityId: payload.iCommunityId,
+            iCategory: payload.iCategory
+         }
+      }).then(res => {
          return res.data;
       }).catch(err => {
          console.log(err);
--- route/community/index.js
+++ route/community/index.js
@@ -2,8 +2,13 @@ const express = require('express');
 const router = express.Router();
 const communityDao = require('../../dao/community');
 
-router.post('/bulletin', async (req, res) => {
-   let result = await communityDao.bulletin(req.body).then(res => res);
+router.post('/announcement_list', async (req, res) => {
+   let result = await communityDao.getBulletin(req.body).then(res => res);
+   res.json(result);
+});
+
+router.post('/notify_list', async (req, res) => {
+   let result = await communityDao.getNotify(req.body).then(res => res);
    res.json(result);
 });
 
