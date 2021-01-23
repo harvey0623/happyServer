086127d77c17203251d90b7c7d408a3f4d0297d6
harvey <harvey@henghung.com>
2021-01-23 10:11:00 +0800

    update cors config

--- index.js
+++ index.js
@@ -19,31 +19,41 @@ app.use(bodyParser.json({
    limit: '50mb'
 }));
 app.use(cookieParser());
-// app.use(cors({
-//    origin: 'http://localhost:8080/',
-//    credentials: true,
-//    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
-//    allowedHeaders: 'Content-Type,Authorization',
-// }));
+
+app.use(cors({
+   origin: 'http://localhost:5500',
+   methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+   credentials: true
+}));
 app.use('/', express.static(path.join(__dirname, 'public')));
 
-app.use(function (req, res, next) {
+// app.use(function (req, res, next) {
 
-   // Website you wish to allow to connect
-   res.setHeader('Access-Control-Allow-Origin', 'http://localhost:8080');
+//    // Website you wish to allow to connect
+//    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:5500');
 
-   // Request methods you wish to allow
-   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+//    // Request methods you wish to allow
+//    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
 
-   // Request headers you wish to allow
-   res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+//    // Request headers you wish to allow
+//    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
 
-   // Set to true if you need the website to include cookies in the requests sent
-   // to the API (e.g. in case you use sessions)
-   res.setHeader('Access-Control-Allow-Credentials', true);
+//    // Set to true if you need the website to include cookies in the requests sent
+//    // to the API (e.g. in case you use sessions)
+//    res.setHeader('Access-Control-Allow-Credentials', true);
 
-   // Pass to next layer of middleware
-   next();
+//    // Pass to next layer of middleware
+//    next();
+// });
+
+app.get('/getCookie', (req, res) => {
+   res.cookie('user', 'harvey');
+   res.json({ status: true })
+});
+
+app.post('/sendCookie', (req, res) => {
+   console.log(req.cookies);
+   res.json({ true: true });
 });
 
 app.use('/auth', auth);
